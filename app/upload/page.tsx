d43be2f4913daacc0f7b595b8774fbcb24ea28d6
Myat Su Mon <myatsumon2531999@gmail.com>
2023-09-30 15:17:31 +0630

    chore: customizing cloudinary upload widget

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,15 +1,37 @@
 "use client";
-import { CldUploadWidget } from "next-cloudinary";
+import { Button } from "@/components/ui/button";
+import { CldImage, CldUploadWidget } from "next-cloudinary";
+import { useState } from "react";
+
+interface CloudinaryResult {
+  public_id: string;
+}
 
 const UploadPage = () => {
+  const [publicId, setPublicId] = useState("");
+
   return (
-    <CldUploadWidget uploadPreset="uqeebh12">
-      {({ open }) => (
-        <button className="btn btn-primary" onClick={() => open()}>
-          Upload
-        </button>
+    <>
+      {publicId && (
+        <CldImage src={publicId} width={300} height={300} alt="nature" />
       )}
-    </CldUploadWidget>
+      <CldUploadWidget
+        uploadPreset="uqeebh12"
+        options={{
+          sources: ["local", "camera"],
+          multiple: false,
+          maxFiles: 5,
+        }}
+        onUpload={(result, widget) => {
+          console.log(result, widget);
+          if (result.event !== "success") return;
+          const info = result.info as CloudinaryResult;
+          setPublicId(info.public_id);
+        }}
+      >
+        {({ open }) => <Button onClick={() => open()}>Upload</Button>}
+      </CldUploadWidget>
+    </>
   );
 };
 
