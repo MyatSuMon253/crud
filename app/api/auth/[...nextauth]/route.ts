0be197e53156b26a9ff7a53f86b27ae9ad6de9ea
Myat Su Mon <myatsumon2531999@gmail.com>
2023-10-03 20:00:27 +0630

    feat: configuring credentials provider

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,31 +1,41 @@
 import { prisma } from "@/app/modules/db";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
 import NextAuth, { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 
 export const AuthOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     CredentialsProvider({
-      name: 'Credentials',
+      name: "Credentials",
       credentials: {
-        email: {label: 'Email', type: 'email', placeholder: 'Email'},
-        password: {label: 'Password', type: 'password', placeholder: 'Password'}
-      }, 
+        email: { label: "Email", type: "email", placeholder: "Email" },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Password",
+        },
+      },
       async authorize(credentials, req) {
         if (!credentials?.email || !credentials.password) return null;
 
-        const user = await prisma.user.findUnique({where: {
-          email: credentials.email
-        }});
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
 
-        if (!user) return null
+        if (!user) return null;
 
-        const passwordsMatch = await bcrypt.compare(credentials.password, user.hashedPassword!);
+        const passwordsMatch = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword!
+        );
 
-        return passwordsMatch ? user: null;
-      }
+        return passwordsMatch ? user : null;
+      },
     }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
