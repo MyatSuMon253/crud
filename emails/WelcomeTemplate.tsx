e89a6417d3edc1fbf88ca16d0f36ab8b6b5c9655
Myat Su Mon <myatsumon2531999@gmail.com>
2023-10-03 22:21:32 +0630

    chore: styling emails

--- emails/WelcomeTemplate.tsx
+++ emails/WelcomeTemplate.tsx
@@ -4,21 +4,33 @@ import {
   Html,
   Link,
   Preview,
-  Text,
+  Tailwind,
+  Text
 } from "@react-email/components";
+import { CSSProperties } from "react";
 
 const WelcomeTemplate = ({ name }: { name: string }) => {
   return (
     <Html>
       <Preview>Welcome Aboard!</Preview>
-      <Body>
+      <Tailwind>
+      <Body className="bg-white">
         <Container>
-          <Text>Hello {name}</Text>
+          <Text className="font-bold text-3xl">Hello {name}</Text>
           <Link href="www.google.com">Google</Link>
         </Container>
       </Body>
+      </Tailwind>
     </Html>
   );
 };
+
+const body: CSSProperties = {
+  background: '#fff';
+}
+
+const heading: CSSProperties = {
+  fontSize: '32px'
+}
 
 export default WelcomeTemplate;
