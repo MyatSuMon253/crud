2d9c9b3842647b8e75668d8f3be2b6559f4bcddf
Myat Su Mon <myatsumon2531999@gmail.com>
2023-10-05 07:48:08 +0630

    feat: lazy loading with lodash

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,39 @@
-import { Metadata } from "next";
-import { getServerSession } from "next-auth";
-import { AuthOptions } from "./api/auth/[...nextauth]/route";
+"use client";
+// import HeavyComponent from "@/components/HeavyComponent";
+import _ from "lodash";
+import { useState } from "react";
 
-export default async function Home() {
-  const session = await getServerSession(AuthOptions);
+// const HeavyComponent = dynamic(() => import("../components/HeavyComponent"), {
+//   ssr: false,
+//   loading: () => <p>Loading...</p>,
+// });
+
+export default function Home() {
+  const [visible, setVisible] = useState(false);
 
   return (
     <main className="relative h-screen">
-      <h1>
-        Hello {session && <span>{session.user!.name}</span>}
-      </h1>
-
-      {/* <Image
-        src="https://bit.ly/react-cover"
-        alt="project photo"
-        fill
-        className="object-cover"
-        sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw"
-        quality={75}
-        priority
-      /> */}
+      <h1>Hello </h1>
+      <button className="bg-yellow-400" onClick={async () => {
+        const x = (await import ('lodash')).default;
+        console.log(x);
+        const users = [
+          {name: 'c'},
+          {name: 'b'},
+          {name: 'a'}
+        ]
+        const sorted = _.orderBy(users, ['name']);
+        console.log(sorted);
+      }} >Show</button>
+      {/* {visible && <HeavyComponent />} */}
     </main>
   );
 }
 
-export async function generateMetadata(): Promise<Metadata> {
-  const product = await fetch('');
-  return {
-    title: 'product.title',
-    description: "..."
-  }
-}
+// export async function generateMetadata(): Promise<Metadata> {
+//   const product = await fetch("");
+//   return {
+//     title: "product.title",
+//     description: "...",
+//   };
+// }
---
+++ components/HeavyComponent.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+const HeavyComponent = () => {
+  return (
+    <div>My HeavyComponent</div>
+  )
+}
+
+export default HeavyComponent
